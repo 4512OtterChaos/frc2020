e0ddd3659c64c8ff58fabac3af6731dea212f019
Nolan <noleetarrr@gmail.com>
2020-02-17 00:31:02 -0800

    math roundToBound function

--- src/main/java/frc/robot/util/MathHelp.java
+++ src/main/java/frc/robot/util/MathHelp.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpiutil.math.MathUtil;
 public final class MathHelp {
     
     // Steal clamp from MathUtil to reduce scope
-
     /**
      * Returns value clamped between low and high boundaries.
      *
@@ -26,7 +25,6 @@ public final class MathHelp {
     public static int clamp(int value, int low, int high) {
       return Math.max(low, Math.min(value, high));
     }
-
     /**
      * Returns value clamped between low and high boundaries.
      *
@@ -37,6 +35,25 @@ public final class MathHelp {
     public static double clamp(double value, double low, double high) {
       return Math.max(low, Math.min(value, high));
     }
+
+    /**
+     * Returns value rounded to either the low or high boundary.
+     *
+     * @param value Value to round.
+     * @param low   The lower boundary to which to round value.
+     * @param high  The higher boundary to which to round value.
+     */
+    public static double roundToBounds(double value, double low, double high){
+      value = clamp(value, low, high);
+      double average = (low+high)/2.0;
+      return (value >= average ? high : low);
+    }
+    public static int roundToBounds(int value, int low, int high){
+      value = clamp(value, low, high);
+      double average = (low+high)/2.0;
+      return (value >= average ? high : low);
+    }
+
     /**
      * Linearly interpolates between a and b by percent amount.
      * @param percent (0-1): 0 = a, 1 = b
