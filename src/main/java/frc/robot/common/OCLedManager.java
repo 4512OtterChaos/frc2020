09d8c85d86db0d41a55b501c20f07e88ca335682
4512 Otter Chaos <otterchaos4512@gmail.com>
2021-03-31 15:43:10 -0700

    Revert "rename"
    This reverts commit d36014e94946a54acfe15f14e16b361097f1c477.

--- src/main/java/frc/robot/common/OCLedManager.java
+++ src/main/java/frc/robot/common/OCLedManager.java
@@ -23,34 +23,34 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 * 
 */
 public class OCLEDManager {
-    
+
     private static OCLEDManager instance;
-    
+
     public static OCLEDManager getInstance(){
         return instance;
     }
-    
+
     public enum Configuration{
         NONE, // normal
         COPY, // normal, but copy first half onto second 
         MIRROR // start patterns from both ends and meet in the middle
     }
-    
+
     private TreeMap<PatternCard, LEDPattern> patternList = new TreeMap<PatternCard, LEDPattern>(new Comparator<PatternCard>(){
         public int compare(PatternCard a, PatternCard b){
             return Integer.compare(a.getPriority(), b.getPriority());
         };
     });
-    
+
     private AddressableLED led;
     private AddressableLEDBuffer buffer;
     private Configuration config;
-    
+
     /**
-    * Create an LED manager for a new LED strip.
-    * @param ledPort PWM port of LED strip
-    * @param length Pixel length of strip
-    */
+     * Create an LED manager for a new LED strip.
+     * @param ledPort PWM port of LED strip
+     * @param length Pixel length of strip
+     */
     public OCLEDManager(int ledPort, int length, Configuration config){
         instance = this;
         this.config = config;
@@ -59,54 +59,54 @@ public class OCLEDManager {
         buffer = new AddressableLEDBuffer(length);
         led.setData(buffer);
         led.start();
-        
+
         addPattern(new PatternCard(()->false, -1), new LEDPattern(this).presetAutomaticRollingWaves(20)); // if no other patterns, we display this pattern by default
     }
-    
+
     
     /**
-    * Set whether patterns are in a split-configuration(start patterns from both ends and meet in the middle).
-    * Useful on strips visible from two sides of the robot(e.g. two sides of an elevator).
-    */
+     * Set whether patterns are in a split-configuration(start patterns from both ends and meet in the middle).
+     * Useful on strips visible from two sides of the robot(e.g. two sides of an elevator).
+     */
     /*
     public void configure(Configuration config){
         this.config = config;
     }
     */
-    
-    /**
-    * @return Whether the LED strip is in split-configuration(start patterns from both ends and meet in the middle).
-    */
+
+     /**
+     * @return Whether the LED strip is in split-configuration(start patterns from both ends and meet in the middle).
+     */
     public Configuration getConfig(){
         return config;
     }
-    
+
     /**
-    * @return Total pixel length of the LED strip
-    */
+     * @return Total pixel length of the LED strip
+     */
     public int getTotalStripLength(){
         return buffer.getLength();
     }
     /**
-    * @return "Configured" length of LED strip(e.g. half total length when in split-configuration)
-    */
+     * @return "Configured" length of LED strip(e.g. half total length when in split-configuration)
+     */
     public int getConfiguredStripLength(){
         // halve the strip depending on config
         return 
-        (config == Configuration.MIRROR ||
-        config == Configuration.COPY) ? buffer.getLength()/2 : buffer.getLength();
+            (config == Configuration.MIRROR ||
+            config == Configuration.COPY) ? buffer.getLength()/2 : buffer.getLength();
     }
-    
+
     public void addPattern(PatternCard card, LEDPattern pattern){
         if(patternList.containsValue(pattern)){ // refresh current patterns
             for(Entry<PatternCard, LEDPattern> entry : patternList.entrySet()){
                 if(entry.getValue().equals(pattern) && entry.getKey().getDuration() > 0)
-                entry.getKey().extend(card.getDuration());
+                    entry.getKey().extend(card.getDuration());
             }
         }
         else patternList.put(card, pattern); // add new patterns
     }
-    
+
     public void periodic(){
         List<PatternCard> toRemove = new ArrayList<PatternCard>();
         patternList.forEach((card, pattern) -> {
@@ -118,27 +118,27 @@ public class OCLEDManager {
         buffer = patternList.lastEntry().getValue().draw();
         led.setData(buffer);
     }
-    
+
     //-----
-    
-    
+
+
     /**
-    * For giving {@link LEDPattern}s a priority and duration.
-    * Call end() to manually end a pattern.
-    */
+     * For giving {@link LEDPattern}s a priority and duration.
+     * Call end() to manually end a pattern.
+     */
     public class PatternCard {
         private final double timeStart;
         private double duration;
         private final BooleanSupplier interrupt; // if interrupted, end pattern
         private final int priority;
         private boolean finished = false; // end pattern based on this boolean
-        
+
         /**
-        * Create a new PatternCard for an associated {@link LEDPattern} that lasts <b>duration</b> seconds and
-        * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
-        * @param duration
-        * @param priority
-        */
+         * Create a new PatternCard for an associated {@link LEDPattern} that lasts <b>duration</b> seconds and
+         * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
+         * @param duration
+         * @param priority
+         */
         public PatternCard(double duration, int priority){
             timeStart = Timer.getFPGATimestamp();
             this.duration = duration;
@@ -146,11 +146,11 @@ public class OCLEDManager {
             this.priority = priority;
         }
         /**
-        * Create a new PatternCard for an associated {@link LEDPattern} that lasts until <b>interrupt</b> is true and
-        * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
-        * @param duration
-        * @param priority
-        */
+         * Create a new PatternCard for an associated {@link LEDPattern} that lasts until <b>interrupt</b> is true and
+         * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
+         * @param duration
+         * @param priority
+         */
         public PatternCard(BooleanSupplier interrupt, int priority){
             timeStart = Timer.getFPGATimestamp();
             this.duration = -1;
@@ -158,44 +158,44 @@ public class OCLEDManager {
             this.priority = priority;
         }
         /**
-        * Create a new PatternCard for an associated {@link LEDPattern} that lasts <b>duration</b> seconds or until <b>interrupt</b> is true and
-        * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
-        * @param duration
-        * @param priority
-        */
+         * Create a new PatternCard for an associated {@link LEDPattern} that lasts <b>duration</b> seconds or until <b>interrupt</b> is true and
+         * has a display <b>priority</b> over other, lower priorities(create an indefinite PatternCard with priority -1 for the base/default LEDPattern).
+         * @param duration
+         * @param priority
+         */
         public PatternCard(double duration, BooleanSupplier interrupt, int priority){
             timeStart = Timer.getFPGATimestamp();
             this.duration = duration;
             this.interrupt = interrupt;
             this.priority = priority;
         }
-        
+
         public int getPriority(){
             return priority;
         }
-        
+
         public double getDuration(){
             return duration;
         }
-        
+
         public void extend(double time){
             duration += time;
         }
-        
+
         /**
-        * Manually end this pattern.
-        */
+         * Manually end this pattern.
+         */
         public void end(){
             finished = true;
         }
-        
+
         /**
-        * Check if this pattern should be ended
-        */
+         * Check if this pattern should be ended
+         */
         public boolean isFinished(){
             if(interrupt.getAsBoolean()) end();
             if(duration > 0 && Timer.getFPGATimestamp() > timeStart + duration) end();
-            
+
             return finished;
         }
     }
